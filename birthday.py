887438857ca958da5fe4c892d6cc413c1e511c7d
Garbolino <Nathan.Garbolino@rjuhsd.int>
2022-01-25 12:43:58 -0800

    Switched to python because running java no work :)

---
+++ birthday.py
@@ -0,0 +1,8 @@
+import datetime
+
+print("I have stolen your IP address")
+m = input("Enter your birth month: ")
+d = input("Enter your birth day: ")
+y = input("Enter your birth year: ")
+
+print("You were born on:", m, "/", d, "/", y)
