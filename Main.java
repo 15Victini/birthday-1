377fb757cd4e60975c2edea72e8f6243c72ade9a
Garbolino <Nathan.Garbolino@rjuhsd.int>
2022-01-25 12:26:04 -0800

    added birthday code

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void Main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your IP address is mine now :)");
+        System.out.println("Enter your birthday (MM/DD/YYYY)");
+        String bday = scan.next();
+    }
+}
